f2506390bd636b3a6b2439236c92bedbbc84c4d2
Ahmed Gado <Ahmed.Gaddo@gmail.com>
2021-03-07 19:29:29 +0200

    Initial Commit
    First commit to put the project under git control

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] float rotFactor;
+
+    Vector3 movementVector;
+    float rotDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movementVector = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.z = Input.GetAxis("Vertical");
+        rotDir = Input.GetKey(KeyCode.Q) ? -1f : Input.GetKey(KeyCode.E) ? 1f : 0;
+    }
+
+
+    private void FixedUpdate()
+    {
+        MovementByPhysics();
+        RotateByAngularVelocit(); 
+    }
+
+    void MovementByPhysics()
+    {
+        rb.velocity = ((((transform.right * movementVector.x) + (transform.forward * movementVector.z)) * speed * Time.fixedDeltaTime) + Vector3.up * rb.velocity.y);
+    }
+    void RotateByAngularVelocit()
+    {
+        rb.angularVelocity = Vector3.up * rotDir * rotFactor;
+    }
+}
