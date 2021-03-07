ba01ac5b753e83d31589d5a743549930fe521339
Ahmed Gado <Ahmed.Gaddo@gmail.com>
2021-03-07 20:58:09 +0200

    Testing gravity velocity and euler number problem on collision enter with ground

--- Assets/Jump System/Scripts/JumpSystem.cs
+++ Assets/Jump System/Scripts/JumpSystem.cs
@@ -12,7 +12,7 @@ public class JumpSystem : MonoBehaviour
     [SerializeField] float delayBeforeJump;
     [SerializeField] bool jumpBreaking;
     [SerializeField] float jumpHeight;
-    [SerializeField] [Range(0f,1f)] float jumpHeightBreakThreeshold;
+    [SerializeField] [Range(0f, 1f)] float jumpHeightBreakThreeshold;
     [SerializeField] bool stopAtPeak;
     [SerializeField] bool isGrounded = true;
     [SerializeField] float gravity;
@@ -26,10 +26,10 @@ public class JumpSystem : MonoBehaviour
     [SerializeField] bool extraJumps;
     [SerializeField] int maxExtraJumps;
     [SerializeField] int jumpCounter;
-    [SerializeField] float allowedFallingVelToMultipleJump; 
+    [SerializeField] float allowedFallingVelToMultipleJump;
     [SerializeField] float multipleJumpMultiplier;
     [SerializeField] float extraJumpHeight;
-    [SerializeField]bool landing;
+    [SerializeField] bool landing;
 
     // Start is called before the first frame update
     void Start()
@@ -43,7 +43,7 @@ public class JumpSystem : MonoBehaviour
     {
         velTxt.text = rb.velocity.y.ToString();
         if (Input.GetButtonDown("Jump") && !IsInvoking(nameof(Jump)))
-        { 
+        {
             if (isGrounded && !landing)
                 if (!waitBeforeJump)
                 {//you can use bool for other scripts to know if this gonna wait before jumping
@@ -58,12 +58,12 @@ public class JumpSystem : MonoBehaviour
 
             else if (extraJumps && jumpCounter <= maxExtraJumps && !(rb.velocity.y < allowedFallingVelToMultipleJump * maxExtraJumps))
             {
-                 //better to use breaking with multiple jump for limiting the sec jump whenever it starts
+                //better to use breaking with multiple jump for limiting the sec jump whenever it starts
                 //jumpCounter++;
                 MultipleJump(jumpCounter);
             }
         }
-         
+
         if (landing && Input.GetButton("Jump"))
         {
             DragResistance(dragResistanceValue);
@@ -119,10 +119,10 @@ public class JumpSystem : MonoBehaviour
             //} 
             #endregion
             if (rb.velocity.y > maxFallingVelocity) //momkn tb2a tdrbha f l fallingVelocity  //to add more force
-            { 
+            {
                 //ApplyGravity(landingVelocity);
                 ApplyGravity(fallingFactor);
-                 
+
                 if (rb.velocity.y < 0)//so u can fall slower only on landing
                     landing = true;
             }
@@ -159,31 +159,32 @@ public class JumpSystem : MonoBehaviour
             //stopAtPeak = false;
         }
     }
-    float GetCurrentTotalJumpHeight() { //not genral height but instatneus height 
+    float GetCurrentTotalJumpHeight()
+    { //not genral height but instatneus height 
         //Debug.Log(jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter- 1f)) : 0.0f));
 
         //return jumpHeight + (extraJumps?(extraJumpHeight * (jumpCounter)):0); //u can make it based on array or collecions
-        return jumpHeight + (extraJumps?(extraJumpHeight * (jumpCounter-1f)):0); //badl l line elly fo2
+        return jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter - 1f)) : 0); //badl l line elly fo2
     }
     void Jump()
     {
-        jumpCounter++; 
+        jumpCounter++;
         rb.velocity += Vector3.up * jumpForce;
-    } 
+    }
     void DoubleJump()
     {
         rb.velocity += Vector3.up * jumpForce * multipleJumpMultiplier;
     }
     void MultipleJump(int jumpNo)
     {
-        jumpCounter++; 
+        jumpCounter++;
         //rb.velocity += Vector3.up * jumpForce * multipleJumpMultiplier * jumpNo;
         rb.velocity += Vector3.up * jumpForce * multipleJumpMultiplier;
         //Debug.Log(String.Format("{0:0.##}", transform.position.y));
 
     }
 
-    void ApplyGravity(float factor =1)
+    void ApplyGravity(float factor = 1)
     {
         //rb.velocity += Vector3.up * gravity * factor;
         rb.AddForce(Vector3.up * gravity * factor, ForceMode.Acceleration);
@@ -192,15 +193,17 @@ public class JumpSystem : MonoBehaviour
 
     void DragResistance(float dragFactor = 1)
     {
-        rb.AddForce(((Vector3.up*0.5f)*(rb.velocity.y*rb.velocity.y))*dragFactor,ForceMode.Acceleration); //from air resistance drag formula
+        rb.AddForce(((Vector3.up * 0.5f) * (rb.velocity.y * rb.velocity.y)) * dragFactor, ForceMode.Acceleration); //from air resistance drag formula
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
-            rb.velocity = new Vector3(rb.velocity.x, 0,rb.velocity.z); //to avoid euler number problem //or do not modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
-            landing = false; 
+            //rb.AddForce(new Vector3(rb.velocity.x, 0, rb.velocity.z), ForceMode.VelocityChange); //to avoid euler number problem //or do not modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
+            //next line is better than above in practice.
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); //to avoid euler number problem //or do not modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
+            landing = false;
             jumpCounter = 0;
         }
     }
@@ -209,9 +212,7 @@ public class JumpSystem : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
-
         }
-
     }
 
 }
