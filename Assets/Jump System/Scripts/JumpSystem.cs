91813ad1415ba50d1f660c57f11c33aaffca99fb
Ahmed Gado <Ahmed.Gaddo@gmail.com>
2021-03-07 23:04:59 +0200

    Cleaning and changing the isGrounded checker

--- Assets/Jump System/Scripts/JumpSystem.cs
+++ Assets/Jump System/Scripts/JumpSystem.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //TODO: Stop At Peak
+//TODO: SphereCast for checking if is ground
+//TODO: 
 public class JumpSystem : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] TMPro.TextMeshProUGUI velTxt;
+    [SerializeField] LayerMask groundLayer;
     [SerializeField] float jumpForce;
     [SerializeField] bool waitBeforeJump;
     [SerializeField] float delayBeforeJump;
@@ -30,12 +33,11 @@ public class JumpSystem : MonoBehaviour
     [SerializeField] float multipleJumpMultiplier;
     [SerializeField] float extraJumpHeight;
     [SerializeField] bool landing;
-
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //landingVelocity = fallingFactor;
     }
 
     // Update is called once per frame
@@ -45,58 +47,43 @@ public class JumpSystem : MonoBehaviour
         if (Input.GetButtonDown("Jump") && !IsInvoking(nameof(Jump)))
         {
             if (isGrounded && !landing)
-                if (!waitBeforeJump)
-                {//you can use bool for other scripts to know if this gonna wait before jumping
+                if (!waitBeforeJump){//you can use bool for other scripts to know if this gonna wait before jumping
                     Jump();
-                    //jumpCounter++; 
                 }
-                else
-                {
-                    Invoke(nameof(Jump), delayBeforeJump);//delay only works with single jump -- update -- now works with first jump
-                                                          //jumpCounter++;
+                else{
+                    Invoke(nameof(Jump), delayBeforeJump);//delay only works with first jump
                 }
 
-            else if (extraJumps && jumpCounter <= maxExtraJumps && !(rb.velocity.y < allowedFallingVelToMultipleJump * maxExtraJumps))
-            {
+            else if (extraJumps && jumpCounter <= maxExtraJumps && !(rb.velocity.y < allowedFallingVelToMultipleJump * maxExtraJumps)){
                 //better to use breaking with multiple jump for limiting the sec jump whenever it starts
-                //jumpCounter++;
                 MultipleJump(jumpCounter);
             }
         }
 
-        if (landing && Input.GetButton("Jump"))
-        {
+        if (landing && Input.GetButton("Jump")){
             DragResistance(dragResistanceValue);
         }
 
-        if (!landing && Input.GetKeyDown(KeyCode.LeftControl))
-        {
+        if (!landing && Input.GetKeyDown(KeyCode.LeftControl)){
             stopAtPeak = true;
         }
-        //if (landing && Input.GetButton("Jump"))
-        //{
-        //    landingVelocity = slowFallingFactor;
-        //}
-        //else if (landingVelocity != fallingFactor)
-        //{
-        //    landingVelocity = fallingFactor;
-        //}
-        //Debug.Log(jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter-1f)) : 0.0f) );
 
     }
     private void FixedUpdate()
     {
-        //if (stopAtPeak)
-        //{
-        //    StopAtPeak();
-        //    Invoke(nameof(ResumeFalling), 2f);
-        //}
-        //else
-        //{ 
-        //Land();
-        //}
+        CheckIsGrounded();
         Land();
     }
+
+    void CheckIsGrounded()
+    { 
+        Debug.DrawRay(transform.position+ Vector3.down * 0.4f, Vector3.down*0.25f,Color.red);
+        //Physics.SphereCast(transform.position + Vector3.down * 0.4f, 2f, Vector3.down, out hit, 0.25f, groundLayer);
+        isGrounded = Physics.Raycast(transform.position + Vector3.down * 0.4f, Vector3.down, 0.25f, groundLayer);
+        //isGrounded = Physics.SphereCast(transform.position + Vector3.down * 0.4f, 2f, Vector3.down, out hit, 2f, groundLayer);
+        if (isGrounded)
+            landing = false; 
+     }
 
     private void Land()
     {
@@ -104,25 +91,9 @@ public class JumpSystem : MonoBehaviour
         {
             if (!stopAtPeak && jumpBreaking)
                 BreakJump();
-            #region old
-            //if (!(rb.velocity.y < 0))//don't use it with breakJump unless you need that behaviour
-            //{
-            //    Debug.Log("qq");
-            //    ApplyGravity(landingVelocity);
-            //}
-
-            //else if (rb.velocity.y < 0 && rb.velocity.y > maxFallingVelocity) //momkn tb2a tdrbha f l landingspeed  //to add more force
-            //{
-            //    Debug.Log("ss");
-            //    landing = true;
-            //    //ApplyGravity(landingVelocity); 
-            //} 
-            #endregion
             if (rb.velocity.y > maxFallingVelocity) //momkn tb2a tdrbha f l fallingVelocity  //to add more force
             {
-                //ApplyGravity(landingVelocity);
                 ApplyGravity(fallingFactor);
-
                 if (rb.velocity.y < 0)//so u can fall slower only on landing
                     landing = true;
             }
@@ -132,16 +103,8 @@ public class JumpSystem : MonoBehaviour
     {
         if (transform.position.y > (GetCurrentTotalJumpHeight()) - jumpHeightBreakThreeshold) //-JHBThreeshold to deaccelarate before breaking the limit
         {
-            //Debug.Log(String.Format("{0:0.##}", transform.position.y));
-
-            //if (jumpBreaking && rb.velocity.y > -fallingFactor)
-            if (rb.velocity.y > -fallingFactor)
+              if (rb.velocity.y > -fallingFactor)
                 rb.velocity -= new Vector3(0, jumpBreakingMultiplier * Time.fixedDeltaTime, 0); //jumpbreakingmultiplier -- try one with force accelaeratoon
-
-            //if (stopInPeak) //need to be a single method and need integration with landing noraml method
-            //{
-            //    StopAtPeak();
-            //}
         }
     }
     void ResumeFalling()
@@ -150,21 +113,14 @@ public class JumpSystem : MonoBehaviour
     }
     void StopAtPeak()
     {
-        //if (transform.position.y >= (GetCurrentTotalJumpHeight() - 0.1f)) //- 0.1f to ensure it doesn't exceed the limit with big value
         if (rb.velocity.y > 0 && transform.position.y >= (jumpHeight - 0.1f)) //- 0.1f to ensure it doesn't exceed the limit with big value
-        {
-            //ApplyGravity(0.1f); 
-            //rb.AddForce(Vector3.up * rb.velocity.y, ForceMode.VelocityChange);            
+        {        
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            //stopAtPeak = false;
         }
     }
-    float GetCurrentTotalJumpHeight()
-    { //not genral height but instatneus height 
-        //Debug.Log(jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter- 1f)) : 0.0f));
-
-        //return jumpHeight + (extraJumps?(extraJumpHeight * (jumpCounter)):0); //u can make it based on array or collecions
-        return jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter - 1f)) : 0); //badl l line elly fo2
+    float GetCurrentTotalJumpHeight()//not genral height but instatneus height 
+    {  
+        return jumpHeight + (extraJumps ? (extraJumpHeight * (jumpCounter - 1f)) : 0); //badl l line elly fo2//u can make it based on array or collecions
     }
     void Jump()
     {
@@ -178,7 +134,6 @@ public class JumpSystem : MonoBehaviour
     void MultipleJump(int jumpNo)
     {
         jumpCounter++;
-        //rb.velocity += Vector3.up * jumpForce * multipleJumpMultiplier * jumpNo;
         rb.velocity += Vector3.up * jumpForce * multipleJumpMultiplier;
         //Debug.Log(String.Format("{0:0.##}", transform.position.y));
 
@@ -186,9 +141,7 @@ public class JumpSystem : MonoBehaviour
 
     void ApplyGravity(float factor = 1)
     {
-        //rb.velocity += Vector3.up * gravity * factor;
         rb.AddForce(Vector3.up * gravity * factor, ForceMode.Acceleration);
-        //rb.AddForce(Vector3.up * gravity * factor,ForceMode.Impulse);//use the prev line l7d mat7ot if use mass or not
     }
 
     void DragResistance(float dragFactor = 1)
@@ -199,11 +152,9 @@ public class JumpSystem : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
-            //rb.AddForce(new Vector3(rb.velocity.x, 0, rb.velocity.z), ForceMode.VelocityChange); //to avoid euler number problem //or do not modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
-            //next line is better than above in practice.
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); //to avoid euler number problem //or do not modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
-            landing = false;
+            //isGrounded = true;
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); //to avoid euler number problem //but notice that you shouldn't modify the velocity directly when applying gravity, as this can result in unrealistic behaviour -
+            //landing = false;
             jumpCounter = 0;
         }
     }
